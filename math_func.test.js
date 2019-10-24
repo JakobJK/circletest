821e805e2900a98b83bb11c9f56bde26adc6ad14
JakobJK <jakobjk@gmail.com>
2019-10-24 12:51:14 -0400

    adding divide

--- math_func.test.js
+++ math_func.test.js
@@ -2,6 +2,7 @@ const {
   add,
   subtract,
   multiply,
+  divide,
 } = require('./math_func');
 
 test('Adds 1 + 2 to equal 3', () => expect(add(1, 2)).toBe(3));
@@ -13,3 +14,6 @@ test('Subtract 2 - 1 to equal 1', () => expect(subtract(2, 1)).toBe(1));
 
 
 test('Multiply 10 * 10 to equal 100', () => expect(multiply(10, 10)).toBe(100));
+
+
+test('Divide 100 by 10 to equal 10', () => expect(divide(100, 10)).toBe(10));
