9457d700c86cbd7aef82f7f2c8102879eab7078a
JakobJK <jakobjk@gmail.com>
2019-10-24 12:35:04 -0400

    multiply test

--- math_func.test.js
+++ math_func.test.js
@@ -10,3 +10,6 @@ test('Adds 10 + 45 to equal 55', () => expect(add(10, 45)).toBe(55));
 
 
 test('Subtract 2 - 1 to equal 1', () => expect(subtract(2, 1)).toBe(1));
+
+
+test('Multiply 10 * 10 to equal 100', () => expect(multiply(10, 10)).toBe(100));
